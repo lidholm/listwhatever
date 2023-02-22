2113f785f781dde03992611288fd7e557e29a4a5
Andreas Lidholm <lidholm.andreas@gmail.com>
2023-02-21 19:23:42 -0800

    Move firebase_options

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -6,7 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutterbase/firebase_options.dart';
+import 'package:flutterbase/app/firebase/firebase_options.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
