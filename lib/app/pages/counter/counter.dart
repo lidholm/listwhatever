aafd381c0e0002915db843a278d638dbc1b1a148
Andreas Lidholm <lidholm.andreas@gmail.com>
2023-02-21 18:53:21 -0800

    Move Counter to a pages directory

--- lib/app/pages/counter/counter.dart
+++ lib/app/pages/counter/counter.dart
@@ -5,11 +5,5 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:bloc/bloc.dart';
-
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
-
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
-}
+export 'counter_page.dart';
+export 'cubit/counter_cubit.dart';
