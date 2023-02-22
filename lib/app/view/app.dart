3df5c3c866820715730752a6bdaa43269864f36c
Andreas Lidholm <lidholm.andreas@gmail.com>
2023-02-21 18:38:11 -0800

    Add GoRouter and redirect when logged in/out

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,26 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterbase/app/view/auth.dart';
 import 'package:flutterbase/app/view/auth_page.dart';
-import 'package:flutterbase/l10n/l10n.dart';
+import 'package:flutterbase/app/view/current_user_provider.dart';
+import 'package:flutterbase/app/view/loading_user_page.dart';
+import 'package:flutterbase/app/view/profile_page.dart';
+import 'package:go_router/go_router.dart';
 
-class App extends StatelessWidget {
+// GoRouter configuration
+final routerProvider = Provider(
+  (ref) => GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const AuthPage(),
+      ),
+      GoRoute(
+        path: '/loading_user',
+        builder: (context, state) => const LoadingUserPage(),
+      ),
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) => const ProfilePage(),
+      ),
+      GoRoute(
+        path: '/authgate',
+        builder: (context, state) => const AuthGate(),
+      ),
+    ],
+    redirect: (context, state) {
+      // and then use userChanges to check for changes to if a user is logged in or not
+      final userChanges = ref.watch(userChangesProvider);
+      final currentUser = FirebaseAuth.instance.currentUser;
+
+      print('currentUser: ${currentUser?.email}');
+      print('userChanges: ${userChanges.asData?.value?.email}');
+
+      if (currentUser != null) {
+        print('going to: /profile');
+        return '/profile';
+      }
+      final path = userChanges.when(
+        error: (e, st) => '/error_loading_user',
+        loading: () => '/loading_user',
+        data: (user) => user == null ? '/authgate' : '/profile',
+      );
+      print('going to. $path');
+      return path;
+    },
+  ),
+);
+
+class App extends ConsumerWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
-        ),
-      ),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const AuthPage(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
