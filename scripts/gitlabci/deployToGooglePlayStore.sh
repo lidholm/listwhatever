8d1a1beb5d5ec65fa713d97ecf0d843dafba316d
Andreas Lidholm <lidholm.andreas@gmail.com>
2023-05-06 04:57:33 +0000

    Fix google play deploy

--- scripts/gitlabci/deployToGooglePlayStore.sh
+++ scripts/gitlabci/deployToGooglePlayStore.sh
@@ -4,8 +4,6 @@ export LANG=en_US.UTF-8
 
 alias bundletool='java -jar /bundletool-all.jar'
 
-#cp daggerImport/release.aab .
-
 cd android
 echo "===---===="
 echo "===---===="
@@ -13,5 +11,5 @@ ls -l /builds/lidholm/listanything/app-production-release.aab
 
 java -jar /bundletool-all.jar dump manifest --bundle /builds/lidholm/listanything/app-production-release.aab --xpath /manifest/@android:versionCode
 
-bundle exec fastlane upload_to_play_store --aab /builds/lidholm/listanything/app-production-release.aab --track internal
+bundle exec fastlane upload_to_play_store --aab /builds/lidholm/listanything/app-production-release.aab --track alpha
 echo "===---===="
