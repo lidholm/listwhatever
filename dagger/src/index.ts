ff08c97c741fa499175126c8253a110de48e788c
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-08-10 08:15:41 -0700

    Use example config

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,41 +1,55 @@
-/**
- * A generated module for Listwhatever functions
- *
- * This module has been generated via dagger init and serves as a reference to
- * basic module structure as you get started with Dagger.
- *
- * Two functions have been pre-created. You can modify, delete, or add to them,
- * as needed. They demonstrate usage of arguments and return types using simple
- * echo and grep commands. The functions can be called from the dagger CLI or
- * from one of the SDKs.
- *
- * The first line in this comment block is a short description line and the
- * rest is a long description with more detail on the module's purpose or usage,
- * if appropriate. All modules should have a short description.
- */
 import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 
 @object()
-class Listwhatever {
+class HelloDagger {
   /**
-   * Returns a container that echoes whatever string argument is provided
+   * Publish the application container after building and testing it on-the-fly
    */
   @func()
-  containerEcho(stringArg: string): Container {
-    return dag.container().from("alpine:latest").withExec(["echo", stringArg])
+  async publish(source: Directory): Promise<string> {
+    await this.test(source)
+    return await this.build(source).publish(
+      "ttl.sh/myapp-" + Math.floor(Math.random() * 10000000),
+    )
   }
 
   /**
-   * Returns lines that match a pattern in the files of the provided Directory
+   * Build the application container
    */
   @func()
-  async grepDir(directoryArg: Directory, pattern: string): Promise<string> {
+  build(source: Directory): Container {
+    const build = this.buildEnv(source)
+      .withExec(["npm", "run", "build"])
+      .directory("./dist")
     return dag
       .container()
-      .from("alpine:latest")
-      .withMountedDirectory("/mnt", directoryArg)
-      .withWorkdir("/mnt")
-      .withExec(["grep", "-R", pattern, "."])
+      .from("nginx:1.25-alpine")
+      .withDirectory("/usr/share/nginx/html", build)
+      .withExposedPort(80)
+  }
+
+  /**
+   * Return the result of running unit tests
+   */
+  @func()
+  async test(source: Directory): Promise<string> {
+    return this.buildEnv(source)
+      .withExec(["npm", "run", "test:unit", "run"])
       .stdout()
+  }
+
+  /**
+   * Build a ready-to-use development environment
+   */
+  @func()
+  buildEnv(source: Directory): Container {
+    const nodeCache = dag.cacheVolume("node")
+    return dag
+      .container()
+      .from("node:21-slim")
+      .withDirectory("/src", source)
+      .withMountedCache("/src/node_modules", nodeCache)
+      .withWorkdir("/src")
+      .withExec(["npm", "install"])
   }
 }
