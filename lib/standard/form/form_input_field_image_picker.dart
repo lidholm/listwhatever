53c47db7fe95a289ff00e5b8b8bf9553e6a697bf
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-09-26 11:28:07 -0700

    Working showing example pic on edit

--- lib/standard/form/form_input_field_image_picker.dart
+++ lib/standard/form/form_input_field_image_picker.dart
@@ -33,55 +33,21 @@ class _FormInputFieldImagePickerState<T>
 
   @override
   Widget build(BuildContext context) {
-    final firebaseStorage = context.watch(getFirebaseStorage());
+    final initialValue = [
+      'https://images.pexels.com/photos/7078045/pexels-photo-7078045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+    ];
+    logger.i('$className: initialValue: $initialValue');
 
-// Create a Bloc for reading images from Firebase storage.
-// One LoadImage event, and a ImageLoaded status
-// And then listen to that
-
-    return FutureBuilder(
-      future: getFirebaseStorage(),
-      builder: (context, snapshot) {
-        final firebaseStorage = snapshot.data;
-        if (firebaseStorage == null) {
-          return Container();
-        }
-        final imageUrlFuture = firebaseStorage
-            .ref()
-            .child('images')
-            .child(widget.field.currentValue ?? defaultImageFilename)
-            .getDownloadURL();
-
-        return FutureBuilder(
-            future: imageUrlFuture,
-            builder: (context, imageSnapshot) {
-              final imageUrl = imageSnapshot.data;
-              logger.i('$className: imageUrl: $imageUrl');
-              final initialValue =
-                  // imageUrl != null
-                  //     ?
-                  [
-                //         imageUrl
-                'https://images.pexels.com/photos/7078045/pexels-photo-7078045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-              ]
-                  // : <String>[]
-                  ;
-              logger.i('$className: initialValue: $initialValue');
-
-              return FormBuilderImagePicker(
-                name: 'singlePhotoWithDecoration',
-                displayCustomType: (obj) =>
-                    obj is ApiImage ? obj.imageUrl : obj,
-                decoration: const InputDecoration(
-                  labelText: 'Pick Single Photo With Decoration Visible',
-                ),
-                showDecoration: true,
-                maxImages: 1,
-                previewAutoSizeWidth: true,
-                initialValue: initialValue,
-              );
-            });
-      },
+    return FormBuilderImagePicker(
+      name: 'singlePhotoWithDecoration',
+      displayCustomType: (obj) => obj is ApiImage ? obj.imageUrl : obj,
+      decoration: const InputDecoration(
+        labelText: 'Pick Single Photo With Decoration Visible',
+      ),
+      showDecoration: true,
+      maxImages: 1,
+      previewAutoSizeWidth: true,
+      initialValue: initialValue,
     );
   }
 }
