b1b10fa6bfa335636deb2afc64e28993784dd0df
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-09-27 18:16:38 -0700

    Convert to use Python for firebase functions

--- functions/src/stringUtils.ts
+++
@@ -1,9 +0,0 @@
-export function getRandomString(numberOfCharacters: number): string {
-  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  const charLength = chars.length;
-  let result = '';
-  for (let i = 0; i < numberOfCharacters; i++) {
-    result += chars.charAt(Math.floor(Math.random() * charLength));
-  }
-  return result;
-}
