9508718aa11c481c7c3ede1f0c4c9094b3ab9e80
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-09-27 17:37:26 -0700

    Break out to separate file

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const {onDocumentCreatedFunction} = require("./firestoreFunctions");
 
 // https://firebase.google.com/docs/functions/get-started
 
@@ -10,18 +10,4 @@ exports.helloWorld = onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
-    // Grab the current value of what was written to Firestore.
-    const original = event.data.data().original;
-  
-    // Access the parameter `{documentId}` with `event.params`
-    logger.log("Uppercasing", event.params.documentId, original);
-  
-    const uppercase = original.toUpperCase();
-  
-    // You must return a Promise when performing
-    // asynchronous tasks inside a function
-    // such as writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    return event.data.ref.set({uppercase}, {merge: true});
-  });
+exports.makeuppercase = onDocumentCreatedFunction;
