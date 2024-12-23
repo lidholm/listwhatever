1fd6f0efbd04b4f6061008c4144c9907d3981f1c
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-12-23 11:58:32 +0100

    Start of using a SliverAppBar

--- lib/pages/list/pages/list_page.dart
+++ lib/pages/list/pages/list_page.dart
@@ -1,86 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:listwhatever/pages/list/pages/blurred_backdrop_image.dart';
-import 'package:listwhatever/pages/list/pages/collapsed_app_bar_content.dart';
-import 'package:listwhatever/pages/list/pages/expanded_bar_content.dart';
-import 'package:listwhatever/pages/list/pages/page_body_widget.dart';
 import 'package:listwhatever/pages/lists/models/list_of_things.dart';
 
-class ListPage extends HookWidget {
+class ListPage extends StatefulWidget {
   const ListPage({required this.list, super.key});
-
   final ListOfThings list;
 
   @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
   Widget build(BuildContext context) {
-    const collapsedBarHeight = 60.0;
-    const expandedBarHeight = 400.0;
-
-    final scrollController = useScrollController();
-    final isCollapsed = useState(false);
-    final didAddFeedback = useState(false);
-
-    return NotificationListener<ScrollNotification>(
-      onNotification: (notification) {
-        isCollapsed.value = scrollController.hasClients &&
-            scrollController.offset > (expandedBarHeight - collapsedBarHeight);
-        if (isCollapsed.value && !didAddFeedback.value) {
-          HapticFeedback.mediumImpact();
-          didAddFeedback.value = true;
-        } else if (!isCollapsed.value) {
-          didAddFeedback.value = false;
-        }
-        return false;
-      },
-      child: Stack(
-        children: [
-          BlurredBackdropImage(list: list),
-          CustomScrollView(
-            controller: scrollController,
-            slivers: [
-              SliverAppBar(
-                expandedHeight: expandedBarHeight,
-                collapsedHeight: collapsedBarHeight,
-                centerTitle: false,
-                pinned: true,
-                title: AnimatedOpacity(
-                  duration: const Duration(milliseconds: 200),
-                  opacity: isCollapsed.value ? 1 : 0,
-                  child: CollapsedAppBarContent(list: list),
-                ),
-                elevation: 0,
-                backgroundColor:
-                    isCollapsed.value ? Colors.black : Colors.transparent,
-                leading: const BackButton(
-                  color: Colors.white,
-                ),
-                flexibleSpace: FlexibleSpaceBar(
-                  background: ExpandedAppBarContent(
-                    list: list,
-                  ),
-                ),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                'assets/images/restaurants.jpeg',
+                fit: BoxFit.cover,
               ),
-              SliverToBoxAdapter(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minHeight: MediaQuery.of(context).size.height,
-                  ),
-                  child: Material(
-                    elevation: 7,
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(
-                        15,
-                      ),
-                      topRight: Radius.circular(
-                        15,
-                      ),
-                    ),
-                    child: PageBodyWidget(list: list),
-                  ),
-                ),
+            ),
+            backgroundColor: Colors.black,
+            centerTitle: true,
+            title: Text(
+              widget.list.name,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
               ),
-            ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return ListTile(
+                  title: Text('Item ${index + 1}'),
+                );
+              },
+              childCount: 20, // Number of items in the list
+            ),
           ),
         ],
       ),
