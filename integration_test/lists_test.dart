0d65bad870098f5517ce78475d6a79cb3a94f3a4
Andreas Lidholm <lidholm.andreas@gmail.com>
2024-12-25 12:41:07 +0100

    Test for shimmers

--- integration_test/lists_test.dart
+++ integration_test/lists_test.dart
@@ -6,7 +6,9 @@ import 'package:integration_test/integration_test.dart';
 import 'package:listwhatever/app/view/app.dart';
 import 'package:listwhatever/auth/auth_repository.dart';
 import 'package:listwhatever/auth/models/authentication_user.dart';
+import 'package:listwhatever/components/shimmer/shimmer_loading.dart';
 import 'package:listwhatever/pages/list/repository/list_item_repository.dart';
+import 'package:listwhatever/pages/lists/components/user_list_tile.dart';
 import 'package:listwhatever/pages/lists/models/user_list.dart';
 import 'package:listwhatever/pages/lists/repository/list_repository.dart';
 import 'package:listwhatever/pages/lists/repository/user_list_repository.dart';
@@ -71,6 +73,28 @@ void main() {
 
     tearDown(() async {
       // Close any open connections, cancel timers, etc.  For example:
+    });
+
+    testWidgets('verify that shimmers show up', (tester) async {
+      await tester.pumpWidget(
+        App(
+          authRepository: authRepository,
+          userListRepository: userListRepository,
+          listRepository: listRepository,
+          listItemRepository: listItemRepository,
+          key: const Key('appkey'),
+        ),
+      );
+      // await tester.pump(const Duration(milliseconds: 1));
+
+      final tile1 = find.text('First list');
+      expect(tile1, findsNothing);
+
+      final userListTiles = find.byType(UserListTile);
+      expect(userListTiles, findsExactly(4));
+
+      final shimmers = find.byType(ShimmerLoading);
+      expect(shimmers, findsExactly(4 * 3));
     });
 
     testWidgets('verify that list of UserLists show up', (tester) async {
